5065b21eaa0242cf6a529b2938482edcd69e610b
likelili <1515476572@qq.com>
2019-12-27 09:56:24 +0800

    init

---
+++ src/test/java/com/wac/plant/PlantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wac.plant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
